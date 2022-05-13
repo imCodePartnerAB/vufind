ebc61fbccca5cba0f502cec58eeac9b8c4e66fb5
JacobSandin <jacob@imcode.com>
2022-05-13 15:06:40 +0200

    Removing tools that has moved to its own repository

--- lots/tools/cleansolr.sh
+++
@@ -1,2 +0,0 @@
-curl http://localhost:8983/solr/biblio/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
-curl http://localhost:8983/solr/biblio/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
