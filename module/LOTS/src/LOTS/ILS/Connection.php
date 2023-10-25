bbd38b7f1351de3af26e94389b3b23e962764213
Sergey Tkachuk <seohands.com@gmail.com>
2023-10-25 19:59:52 +0300

    module/LOTS/src/LOTS/ILS/Connection.php

--- module/LOTS/src/LOTS/ILS/Connection.php
+++ module/LOTS/src/LOTS/ILS/Connection.php
@@ -70,6 +70,42 @@ class Connection extends \VuFind\ILS\Connection
     }
 
     /**
+     * Check for updateMobileNumber
+     *
+     * A support method for checkFunction(). This is responsible for checking
+     * the driver configuration to determine if the system supports updating phone
+     * number.
+     *
+     * @param array $functionConfig The configuration values
+     * @param array $params         Patron data
+     *
+     * @return mixed On success, array of configuration data; on failure, false.
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function checkMethodupdateMobileNumber($functionConfig, $params)
+    {
+        if (!isset($functionConfig['method'])) {
+            return false;
+        }
+        if ($functionConfig['method'] == 'driver'
+            && !empty($functionConfig['emailAddress'])
+        ) {
+            return $functionConfig;
+        }
+        if ($functionConfig['method'] == 'url' && !empty($functionConfig['url'])) {
+            return $functionConfig;
+        }
+        if ($functionConfig['method'] == 'driver'
+            && $this->checkCapability('updateMobileNumber', [$params ?: []])
+        ) {
+            return $functionConfig;
+        }
+
+        return false;
+    }
+
+    /**
      * Check for updateSmsNumber
      *
      * A support method for checkFunction(). This is responsible for checking
