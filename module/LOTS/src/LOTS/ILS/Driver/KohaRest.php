09b0cb7fca2d694e52d1017efccd5f92aeb2a59b
Sergey Tkachuk <seohands.com@gmail.com>
2023-10-25 20:05:04 +0300

    module/LOTS/src/LOTS/ILS/Driver/KohaRest.php

--- module/LOTS/src/LOTS/ILS/Driver/KohaRest.php
+++ module/LOTS/src/LOTS/ILS/Driver/KohaRest.php
@@ -37,6 +37,30 @@ class KohaRest extends \VuFind\ILS\Driver\KohaRest
     }
 
     /**
+     * Update patron's Mobile alert number
+     *
+     * @param array  $patron Patron array
+     * @param string $number Mobile alert number
+     *
+     * @throws ILSException
+     *
+     * @return array Associative array of the results
+     */
+    public function updateMobileNumber($patron, $number)
+    {
+        $fields = !empty($this->config['updateMobileNumber']['fields'])
+            ? explode(',', $this->config['updateMobileNumber']['fields'])
+            : ['mobile_number'];
+
+        $update = [];
+        foreach ($fields as $field) {
+            $update[$field] = $number;
+        }
+
+        return $this->updatePatron($patron, $update);
+    }
+
+    /**
      * Update patron's phone number
      *
      * @param array  $patron Patron array
@@ -99,6 +123,7 @@ class KohaRest extends \VuFind\ILS\Driver\KohaRest
             'lastname' => $result['surname'],
             'phone' => $result['phone'],
             'mobile_phone' => $result['mobile'],
+            'sms_number' => $result['sms_number'],
             'email' => $result['email'],
             'address1' => $result['address'],
             'address2' => $result['address2'],
